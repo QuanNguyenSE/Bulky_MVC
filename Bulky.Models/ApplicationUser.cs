4e3abae5f6ec377ac5c5d60da88c9811f6fae4c0
Quan Nguyen <quan.nguyen.se@gmail.com>
2024-09-09 07:57:27 +0700

    User Management

--- Bulky.Models/ApplicationUser.cs
+++ Bulky.Models/ApplicationUser.cs
@@ -17,6 +17,9 @@ namespace Bulky.Models
         public int? CompanyId { get; set; }
         [ForeignKey("CompanyId")]
         [ValidateNever]
-        public Company Company { get; set; }
+        public Company? Company { get; set; }
+        [NotMapped]
+        public string Role { get; set; }
+
     }
 }
